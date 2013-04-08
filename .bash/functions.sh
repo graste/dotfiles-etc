596aa59dd9c908a6da117ade351e286a00ed0cdd
Steffen Gransow <graste@mivesto.de>
2013-04-08 23:31:04 +0200

    added resize-web-image function

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -10,7 +10,22 @@ function remind()
 }
 
 # quick info about data files
-i(){
+function i()
+{
     (head -n 5;tail -n 5) < "$1" | column -t
 }
 
+# resize remote image to local file with given width via curl/webservice
+function resize-web-image()
+{
+    if [ "" = "$3" ]; then
+        echo "Usage: resize-web-image '<image-url>' <width> <outputfile>";
+    else
+        local imageUrl=$1;
+        local width=$2;
+        local outputfile=$3;
+        curl -o $outputfile http://src.sencha.io/$width/$imageUrl;
+    fi
+}
+
+# vim: set ts=4 sw=4 tw=0 et :
